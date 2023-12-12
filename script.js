71e5ec7f05b7162f97c331d0cd81bc1f338de1f0
ARC <arunmareddy@gmail.com>
2023-12-12 11:46:50 +0530

    new commit

--- script.js
+++ script.js
@@ -1,42 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const navItems = document.querySelectorAll('nav a');
-    const logo = document.querySelector('.logo');
-  
-    navItems.forEach(item => {
-      item.addEventListener('mouseover', () => {
-        item.style.color = '#ffffff';
-        item.style.border = '2px solid #ffffff';
-  
-        // Make other items grey
-        navItems.forEach(otherItem => {
-          if (otherItem !== item) {
-            otherItem.style.color = '#7A746F';
-          }
-        });
+  const navItems = document.querySelectorAll('nav a');
+  const logo = document.querySelector('.logo');
+
+  navItems.forEach(item => {
+    item.addEventListener('mouseover', () => {
+      item.classList.add('active-item');
+      navItems.forEach(otherItem => {
+        if (otherItem !== item) {
+          otherItem.classList.add('inactive-item');
+        }
       });
-  
-      item.addEventListener('mouseout', () => {
-        // Revert styles for the hovered item
-        item.style.color = '#ffffff';
-        item.style.border = '2px solid transparent';
-  
-        // Revert styles for other items
-        navItems.forEach(otherItem => {
-          if (otherItem !== item) {
-            otherItem.style.color = '#ffffff';
-          }
-        });
-      });
-    });
-  
-    // Add click event for the logo
-    logo.addEventListener('click', function () {
-      // Set the window location to the home page or refresh the page
-      window.location.href = 'index.html';
-      // Alternatively, use window.location.reload(true) to force a reload
     });
 
-    const tooltipContainer = document.querySelector('.tooltip-container');
+    item.addEventListener('mouseout', () => {
+      item.classList.remove('active-item');
+      navItems.forEach(otherItem => {
+        otherItem.classList.remove('inactive-item');
+      });
+    });
+  });
+
+  // Add click event for the logo
+  logo.addEventListener('click', function () {
+    // Set the window location to the home page or refresh the page
+    window.location.href = 'index.html';
+    // Alternatively, use window.location.reload(true) cautiously
+  });
+
+  const tooltipContainer = document.querySelector('.tooltip-container');
 
   tooltipContainer.addEventListener('mouseover', function () {
     const tooltipText = this.querySelector('.tooltip-text');
@@ -47,5 +38,4 @@ document.addEventListener('DOMContentLoaded', function () {
     const tooltipText = this.querySelector('.tooltip-text');
     tooltipText.style.visibility = 'hidden';
   });
-  });
-  
+});
