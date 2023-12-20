048863bd7b0db39de464450138e4ec1776c3a25f
ARC <arunmareddy@gmail.com>
2023-12-20 15:08:49 +0530

    new latetst changes

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   const navItems = document.querySelectorAll('nav a');
   const logo = document.querySelector('.logo');
-  const menuIcon = document.querySelector('.menu-icon');
 
   navItems.forEach(item => {
     item.addEventListener('mouseover', () => {
@@ -31,23 +30,14 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   // Add click event for the logo
-  logo.addEventListener('click', function () {
-    // Set the window location to the home page or refresh the page
-    window.location.href = 'index.html';
-    // Alternatively, use window.location.reload(true) to force a reload
-  });
+  if (logo) {
+    logo.addEventListener('click', function () {
+      // Set the window location to the home page or refresh the page
+      window.location.href = 'index.html';
+      // Alternatively, use window.location.reload(true) to force a reload
+    });
+  }
 
-  const tooltipContainer = document.querySelector('.tooltip-container');
-
-  tooltipContainer.addEventListener('mouseover', function () {
-    const tooltipText = this.querySelector('.tooltip-text');
-    tooltipText.style.visibility = 'visible';
-  });
-
-  tooltipContainer.addEventListener('mouseout', function () {
-    const tooltipText = this.querySelector('.tooltip-text');
-    tooltipText.style.visibility = 'hidden';
-  });
 
   const helloText = document.querySelector('.hello-text');
 
@@ -67,27 +57,45 @@ document.addEventListener('DOMContentLoaded', function () {
     }, 500); // Adjust the duration as needed
   }
 
-  setInterval(changeHelloText, 2000); // Change greeting every 2 seconds
+  setInterval(changeHelloText, 2000); 
 
 
 const shineElement = document.getElementById('shine');
 
-  document.addEventListener('mousemove', function (event) {
-    // Update shine element position based on mouse coordinates
-    const mouseX = event.clientX;
-    const mouseY = event.clientY;
+  if (shineElement) {
+    document.addEventListener('mousemove', function (event) {
+      // Update shine element position based on mouse coordinates
+      const mouseX = event.clientX;
+      const mouseY = event.clientY;
 
-    shineElement.style.left = `${mouseX - shineElement.offsetWidth / 2}px`;
-    shineElement.style.top = `${mouseY - shineElement.offsetHeight / 2}px`;
+      shineElement.style.left = `${mouseX - shineElement.offsetWidth / 2}px`;
+      shineElement.style.top = `${mouseY - shineElement.offsetHeight / 2}px`;
 
-    // Show the shine element
-    shineElement.style.opacity = 1;
+      // Show the shine element
+      shineElement.style.opacity = 1;
 
-    // Optional: Hide the shine element after a short delay
-    setTimeout(() => {
-      shineElement.style.opacity = 0;
-    }, 200);
-  });
+      // Optional: Hide the shine element after a short delay
+      setTimeout(() => {
+        shineElement.style.opacity = 1;
+      }, 200);
+    });
+  }
 
+
+
+  const downIcon = document.getElementById('downIcon');
+  const tooltipTextDown = document.querySelector('.tooltip-text-down');
+
+  if (downIcon && tooltipTextDown) {
+    downIcon.addEventListener('mouseover', () => {
+      tooltipTextDown.style.visibility = 'visible';
+      tooltipTextDown.style.opacity = 1;
+    });
+
+    downIcon.addEventListener('mouseout', () => {
+      tooltipTextDown.style.visibility = 'hidden';
+      tooltipTextDown.style.opacity = 0;
+    });
+  }
 
 });
